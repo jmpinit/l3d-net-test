4b634a7461097f35383df17d3397a000204cdd22
Owen Trueblood <trueowen@verizon.net>
2015-03-03 14:19:10 -0500

    Basic server example.

--- firmware/tests/pc-to-cube.cpp
+++ firmware/tests/pc-to-cube.cpp
@@ -1,15 +1,36 @@
 #include "l3d-cube/l3d-cube.h"
 #include <math.h>
 
+TCPServer server = TCPServer(23);
+
 Cube cube = Cube();
 
 void setup()
 {
+  server.begin();
+
   cube.begin();
   cube.background(black);
+
+  Serial.begin(9600);
+
+  while(!Serial.available()) SPARK_WLAN_Loop();
+
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.subnetMask());
+  Serial.println(WiFi.gatewayIP());
+  Serial.println(WiFi.SSID());
 }
 
 void loop()
 {
-    // TODO
+  if (client.connected()) {
+    // echo all available bytes back to the client
+    while (client.available()) {
+      server.write(client.read());
+    }
+  } else {
+    // if no client is yet connected, check for a new connection
+    client = server.available();
+  }
 }
